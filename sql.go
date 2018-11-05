a32954b9f12fdd27a12b39377639b95c1817679c
kp0z <dangkhoa.129@gmail.com>
2018-11-05 21:46:11 +1000

    sql insert w context

--- sql.go
+++ sql.go
@@ -25,3 +25,31 @@ func queryRow(ctx context.Context, db *sql.DB, id int) (string, error) {
 	err := db.QueryRowContext(ctx, "SELECT name FROM users WHERE id = ?").Scan(&name)
 	return name, err
 }
+
+func insertRows(ctx context.Context, db *sql.DB, rows []string) error {
+	tx, err := db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.PrepareContext(ctx, "INSERT INTO users (name) VALUES (?)")
+	if err != nil {
+		return err
+	}
+
+	for _, row := range rows {
+		_, err := stmt.Exec(row)
+		if err != nil {
+			if err = tx.Rollback(); err != nil {
+				return err
+			}
+			return err
+		}
+	}
+
+	if err = tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
