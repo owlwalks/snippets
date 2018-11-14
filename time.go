cb834afe489582ab7689cca50ad8c077fefc6c21
kp0z <dangkhoa.129@gmail.com>
2018-11-14 20:25:57 +1000

    parse js dt and timers

---
+++ time.go
@@ -0,0 +1,28 @@
+package snippets
+
+import (
+	"fmt"
+	"time"
+)
+
+func everySec() {
+	c := time.Tick(1 * time.Second)
+	for now := range c {
+		fmt.Printf("%v\n", now)
+	}
+}
+
+func afterOneSec() {
+	select {
+	case <-time.After(1 * time.Second):
+		fmt.Println("timed out")
+	}
+}
+
+func parseJsDt(t string) (time.Time, error) {
+	pt, err := time.Parse("Mon Jan 02 2006 15:04:05 GMT-0700 (MST)", t)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return pt.UTC(), nil
+}
